80cf624ef45248e0494795ec1299fd77fefc9d64
hourtt <bunhourtjesda@gmail.com>
2024-08-29 21:48:43 +0700

    netflix clone

---
+++ lib/api/api.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/movie_model.dart';
+
+class Api {
+  //TODO: Create popularApiUrl and topRatedApiUrl with refering upComingApiUrl. You can check 'get' in TMDB website and add "?api_key=$apiKey"
+  final upComingApiUrl =
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=848b2d83aeff2b2481c84ae8831ffc39";
+  final popularApiUrl =
+      "https://api.themoviedb.org/3/movie/popular?api_key=848b2d83aeff2b2481c84ae8831ffc39";
+  final topRatedApiUrl =
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=848b2d83aeff2b2481c84ae8831ffc39";
+
+  Future<List<Movie>> getUpcomingMovies() async {
+    final response = await http.get(Uri.parse(upComingApiUrl));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['results'];
+
+      List<Movie> movies = data.map((movie) => Movie.fromMap(movie)).toList();
+      return movies;
+    } else {
+      throw Exception('Failed to load upcoming movies');
+    }
+  }
+
+  //TODO: Create getPopularMovies(), getTopRatedMovies() with refering getUpcomingMovies()
+
+  Future<List<Movie>> getPopularMovies() async {
+    final response = await http.get(Uri.parse(popularApiUrl));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['result'];
+      List<Movie> movies = data.map((movie) => Movie.fromMap(movie)).toList();
+      return movies;
+    } else {
+      throw Exception('Failed to load popular movies');
+    }
+  }
+
+  Future<List<Movie>> getTopRatedMovies() async {
+    final response = await http.get(Uri.parse(topRatedApiUrl));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['results'];
+      List<Movie> movies = data.map((movie) => Movie.fromMap(movie)).toList();
+      return movies;
+    } else {
+      throw Exception("Failed to load top rated movies");
+    }
+  }
+}
---
+++ lib/api/constants.dart
@@ -0,0 +1,3 @@
+
+//TODO: Put your api key here
+const apiKey = "848b2d83aeff2b2481c84ae8831ffc39";
