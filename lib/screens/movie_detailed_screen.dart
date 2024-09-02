0ef435554366a7b74f03ccef702c210bd512574a
hourtt <bunhourtjesda@gmail.com>
2024-09-02 20:32:33 +0700

    Movie Detail Screen

--- lib/screens/movie_detailed_screen.dart
+++ lib/screens/movie_detailed_screen.dart
@@ -1,11 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import '../model/movie_model.dart';
 
 class MovieDetailScreen extends StatefulWidget {
   final Movie movie;
-  const MovieDetailScreen({super.key, 
-  required this.movie
-  });
+  const MovieDetailScreen({super.key, required this.movie});
 
   @override
   MovieDetailScreenState createState() => MovieDetailScreenState();
@@ -23,24 +22,76 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
     return Scaffold(
       backgroundColor: Colors.black12,
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-
-          //TODO: Put the back drop image with "https://image.tmdb.org/t/p/original/${widget.movie.backDropPath}". Don't forget there is back button on the image of left top side.
-
-          Padding(
-            padding: const EdgeInsets.only(top: 25, left: 10, right: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          children: [
+            //* Backdrop Image with Back Button
+            Stack(
               children: [
-                //TODO: Put the title, relased date, and overview
-
+                CachedNetworkImage(
+                  imageUrl:
+                      "https://image.tmdb.org/t/p/original/${widget.movie.backDropPath}",
+                  fit: BoxFit.cover,
+                  width: size.width,
+                  height: size.height * 0.4,
+                  // placeholder: (context, url) =>
+                  //     Center(child: CircularProgressIndicator()),
+                  // errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                Positioned(
+                  top: 40,
+                  left: 10,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
               ],
             ),
-          ),
-
-        ],
-      )),
+            //* Movie Details
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //* Movie Title
+                  Text(
+                    widget.movie.title,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  //* Movie Release Date
+                  Text(
+                    "Release Date: ${widget.movie.releaseDate}",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  //* Movie Overview
+                  Text(
+                    widget.movie.overview,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
