fb0033bdc9e09d8d3c0d8355ae8ddcd0e5ae72b0
hourtt <bunhourtjesda@gmail.com>
2024-09-02 20:30:51 +0700

    movie model class

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -2,18 +2,27 @@ class Movie {
   final String title;
   final String backDropPath;
   //TODO: Add overview, posterPath, releaseDate
+  final String overview;
+  final String posterPath;
+  final String releaseDate;
 
-  Movie({
-    required this.title,
-    required this.backDropPath,
-    //TODO: Add overview, posterPath, releaseDate
-  });
+  Movie(
+      {required this.title,
+      required this.backDropPath,
+      //TODO: Add overview, posterPath, releaseDate
+      required this.overview,
+      required this.posterPath,
+      required this.releaseDate
+      });
 
   factory Movie.fromMap(Map<String, dynamic> map) {
     return Movie(
       title: map['title'],
       backDropPath: map['backdrop_path'],
       //TODO: Add overview, posterPath, releaseDate. But you should know you have to use _ for bring data from map. Ex) backDropPath: map['backdropPath'] XXXXX  backDropPath: map['backdrop_path'] OOOO,
+      overview: map['overview'],
+      posterPath: map['posterPath'],
+      releaseDate: map['release_date'],
     );
   }
 
@@ -22,6 +31,9 @@ class Movie {
       'title': title,
       'backDropPath': backDropPath,
       //TODO: Add overview, posterPath, releaseDate.
+      'overview': overview,
+      'posterPath': posterPath,
+      'release_date': releaseDate,
     };
   }
 }
