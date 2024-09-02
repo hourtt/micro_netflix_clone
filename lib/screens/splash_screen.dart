9ce42c0adf750beed61ec685a31f06dce7a7b514
hourtt <bunhourtjesda@gmail.com>
2024-09-02 20:33:53 +0700

    Netflix opening/splash screen when opened the app

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: unused_import
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:netflix_clone/screens/home.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,12 +17,18 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     //TODO: Set the timer to move to Home screen after 4 seconds
+    //* Timer
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        //TODO: Use Littie.asset widget to use lottie file
-        );
+      //TODO: Use Littie.asset widget to use lottie file
+      child: Lottie.asset('images/netflix_intro.json'),
+    );
   }
 }
